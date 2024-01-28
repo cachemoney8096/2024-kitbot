47bf0e7360cdb30bf3448bfc9fa5e3d14bb486c5
SaanikaGW <dutta.saanika@gmail.com>
2024-01-27 19:12:35 -0600

    define the motors and taking values for front and back
    I defined the motors and took  forward/backward values from left joystick and left/right values from right joystick using ArcadeDrive in DifferentialDrive class

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,71 +1,41 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.DoubleConsumer;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Drive extends SubsystemBase{
 
     private int REPLACE_THIS_NUMBER;
 
-    private VictorSPX frontLeftMotor;
-    private VictorSPX frontRightMotor; 
-    private VictorSPX backLeftMotor; 
-    private VictorSPX backRightMotor; 
+    private WPI_VictorSPX frontLeftMotor;
+    private WPI_VictorSPX frontRightMotor; 
+    private WPI_VictorSPX backLeftMotor; 
+    private WPI_VictorSPX backRightMotor; 
 
-    private Joystick forwardandback;
-    private Joystick leftandright;
+    private DifferentialDrive differentialDrive;
 
     public void robotInit(){
-        frontLeftMotor = new VictorSPX(REPLACE_THIS_NUMBER);
-        backLeftMotor = new VictorSPX(REPLACE_THIS_NUMBER);
-        forwardandback = new Joystick(REPLACE_THIS_NUMBER);
-        leftandright = new Joystick(REPLACE_THIS_NUMBER);
-        frontRightMotor= new VictorSPX(REPLACE_THIS_NUMBER);
-        backRightMotor= new VictorSPX(REPLACE_THIS_NUMBER);
-    
+        frontLeftMotor = new WPI_VictorSPX(REPLACE_THIS_NUMBER);
+        backLeftMotor = new WPI_VictorSPX(REPLACE_THIS_NUMBER);
 
+        frontRightMotor= new WPI_VictorSPX(REPLACE_THIS_NUMBER);
+        backRightMotor= new WPI_VictorSPX(REPLACE_THIS_NUMBER);
+        differentialDrive = new DifferentialDrive(frontLeftMotor, frontRightMotor);
+    
     }
 
-    public void teleopPeriodic(){
-
-        double motorSpeed = forwardandback.getRawAxis(REPLACE_THIS_NUMBER);
-        frontLeftMotor.set(ControlMode.PercentOutput, motorSpeed);
-        backLeftMotor.set(ControlMode.PercentOutput,motorSpeed);
-
-        if(forwardandback.getRawButtonPressed(1)){
-            double rampRate = forwardandback.getRawAxis(REPLACE_THIS_NUMBER);
-
-            frontLeftMotor.configOpenloopRamp(rampRate);
-            backLeftMotor.configOpenloopRamp(rampRate);
-        }
-
-        if(forwardandback.getRawAxis(REPLACE_THIS_NUMBER)>0){
-            if(leftandright.getRawAxis(REPLACE_THIS_NUMBER)>0){
-                frontLeftMotor.set(ControlMode.PercentOutput, forwardandback.getRawAxis(REPLACE_THIS_NUMBER));
-            } 
-        }
-
-        if(forwardandback.getRawAxis(REPLACE_THIS_NUMBER)>0){
-            if(leftandright.getRawAxis(REPLACE_THIS_NUMBER)>0){
-                frontRightMotor.set(ControlMode.PercentOutput, forwardandback.getRawAxis(REPLACE_THIS_NUMBER));
-            } 
-        }
-
-        if(forwardandback.getRawAxis(REPLACE_THIS_NUMBER)>0){
-            if(leftandright.getRawAxis(REPLACE_THIS_NUMBER)>0){
-                backLeftMotor.set(ControlMode.PercentOutput, forwardandback.getRawAxis(REPLACE_THIS_NUMBER));
-            } 
-        }
-
-        if(forwardandback.getRawAxis(REPLACE_THIS_NUMBER)>0){
-            if(leftandright.getRawAxis(REPLACE_THIS_NUMBER)>0){
-                backRightMotor.set(ControlMode.PercentOutput, forwardandback.getRawAxis(REPLACE_THIS_NUMBER));
-            } 
-        }
-
+    public void arcadeDrive(double forwardBackward, double rotation ) {
+        differentialDrive.arcadeDrive(forwardBackward, rotation);
     }
 }
 
