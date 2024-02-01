d18047350e2937736bdf901a8c653049848d90b8
Asher Grossman <asher.grossman@gmail.com>
2024-01-31 18:45:26 -0600

    Remove duplicate drive subsystem

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.drivetrain;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Drivetrain extends SubsystemBase {
-    private static VictorSPX leftMotor1 = new VictorSpx(RobotMap.DRIVE_LEFT_MOTOR_1);
-
-}
